89f7e91816fb257c24e97bd023ff530e3aded00c
uysuy <uysuypc@gmail.com>
2017-07-04 11:12:01 +0700

    I added MsgBox on event form Load

--- TTC-Team/Form1.vb
+++ TTC-Team/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Hello World", vbInformation + vbOKOnly, "Testing")
+    End Sub
 End Class
