8e5f9b510efdaa510b0e613b7629d602d8cf1f36
uysuy <uysuypc@gmail.com>
2017-07-04 13:35:37 +0700

    I change name txtID and txtName

--- TTC-Team/Form1.Designer.vb
+++ TTC-Team/Form1.Designer.vb
@@ -22,38 +22,38 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtID
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(158, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.txtID.Location = New System.Drawing.Point(12, 12)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(158, 20)
+        Me.txtID.TabIndex = 0
         '
-        'TextBox2
+        'txtName
         '
-        Me.TextBox2.Location = New System.Drawing.Point(12, 64)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(158, 20)
-        Me.TextBox2.TabIndex = 1
+        Me.txtName.Location = New System.Drawing.Point(12, 64)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(158, 20)
+        Me.txtName.TabIndex = 1
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(502, 297)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtID)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtID As System.Windows.Forms.TextBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
 
 End Class
